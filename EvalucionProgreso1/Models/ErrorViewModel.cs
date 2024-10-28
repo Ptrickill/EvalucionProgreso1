2a75e983d933d490deaf90fcc573421c79a0ee57
laboratorio <laboratorio@UPOAULA10207>
2024-10-28 07:17:25 -0500

    Agregar archivos de proyecto.

---
+++ EvalucionProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EvalucionProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
