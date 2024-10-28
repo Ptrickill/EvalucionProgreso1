2e6ba39e49daefb5d4473d069dc5ed07ca5d1384
ptrickill <daniel.diaz.mosquera8@gmail.com>
2024-10-28 07:57:59 -0500

    QuintoCommit

--- EvalucionProgreso1/Models/Celular.cs
+++ EvalucionProgreso1/Models/Celular.cs
@@ -4,24 +4,6 @@ namespace EvalucionProgreso1.Models
 {
     public class Celular
     {
-        public int Id { get; set; }
-
-        [Required]
-        [Range(1, int.MaxValue)]
-        public int AtributoInt { get; set; }
-
-        [Required]
-        [Range(0.01, int.MaxValue)]
-        public decimal AtributoDecimal { get; set; }
-
-        [Required]
-        [StringLength(100)]
-        public string AtributoString { get; set; }
-
-        [Required]
-        public DateTime AtributoFecha { get; set; }
-
-        public bool AtributoBool { get; set;}
 
 
     }
--- EvalucionProgreso1/Models/Diaz.cs
+++ EvalucionProgreso1/Models/Diaz.cs
@@ -1,6 +1,26 @@
-﻿namespace EvalucionProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvalucionProgreso1.Models
 {
     public class Diaz
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int AtributoInt { get; set; }
+
+        [Required]
+        [Range(0.01, int.MaxValue)]
+        public decimal AtributoDecimal { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string AtributoString { get; set; }
+
+        [Required]
+        public DateTime AtributoFecha { get; set; }
+
+        public bool AtributoBool { get; set; }
     }
 }
