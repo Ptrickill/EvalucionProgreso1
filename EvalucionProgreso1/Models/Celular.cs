8090a15ae789bd795a9be6eed15f2a45d7e71421
ptrickill <daniel.diaz.mosquera8@gmail.com>
2024-10-28 07:28:34 -0500

    Primer commit

---
+++ EvalucionProgreso1/Models/Celular.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvalucionProgreso1.Models
+{
+    public class Celular : Controller
+    {
+        // GET: Celular
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Celular/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Celular/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Celular/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Celular/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Celular/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Celular/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Celular/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
